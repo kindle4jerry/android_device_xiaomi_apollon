4c82cddb2a81a7c37f27dc9dd4cb04b22e879dae
2869145032 <2869145032@qq.com>
2022-10-01 18:41:47 +0800

    add

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-kscope
 
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 29
